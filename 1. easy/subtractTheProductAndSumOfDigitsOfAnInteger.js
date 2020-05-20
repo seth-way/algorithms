b44b52d53cba8aebb1f22bbfb870f5cc6a6281b1
seth-way <seth@Sethaniels-MacBook-Pro.local>
2020-05-20 16:13:03 -0700

    complete solution for 'subtractProductAndSum' - 0(n) time complexity

---
+++ 1. easy/subtractTheProductAndSumOfDigitsOfAnInteger.js
@@ -0,0 +1,45 @@
+/*
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+-----------------------
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+
+-----------------------
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+ 
+-----------------------
+Constraints:
+
+1 <= n <= 10^5
+*/
+
+const intToStringArray = (int) => (
+  int.toString(10).split("")
+);
+
+const productOfDigits = (arr) => (
+  arr.reduce((acc, cur) => (acc * parseInt(cur)), 1)
+);
+
+const sumOfDigits = (arr) => (
+  arr.reduce((acc, cur) => (acc + parseInt(cur)), 0)
+);
+
+const subtractProductAndSum = (n) => {
+  const arr = intToStringArray(n);
+  return productOfDigits(arr) - sumOfDigits(arr);
+};
