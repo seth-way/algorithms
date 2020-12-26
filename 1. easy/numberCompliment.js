c10348f46158ad5ebaea8ed59a66967313616aef
seth-way <sethaniel.way@outlook.com>
2020-12-26 16:13:18 -0500

    complete solution for 'findCompliment' - O(n) time complexity

---
+++ 1. easy/numberCompliment.js
@@ -0,0 +1,48 @@
+/*
+https://leetcode.com/problems/number-complement/
+
+Given a positive integer num, output its complement number.
+The complement strategy is to flip the bits of its binary representation.
+
+---------------------------------------------------------------------------
+
+Example 1:
+-
+Input: num = 5
+Output: 2
+Explanation:
+The binary representation of 5 is 101 (no leading zero bits),
+and its complement is 010. So you need to output 2.
+-
+Example 2:
+-
+Input: num = 1
+Output: 0
+Explanation:
+The binary representation of 1 is 1 (no leading zero bits),
+and its complement is 0. So you need to output 0.
+
+---------------------------------------------------------------------------
+
+Constraints:
+
+The given integer num is guaranteed to fit within the range of a 32-bit signed integer.
+num >= 1
+You could assume no leading zero bit in the integer’s binary representation.
+This question is the same as 1009: https://leetcode.com/problems/complement-of-base-10-integer/
+*/
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const findComplement = (num) => {
+    const binary = num.toString(2);
+    let inverse = '';
+    
+    for (let i = 0; i < binary.length; i += 1) {
+        inverse += binary[i] === '0' ? '1' : '0';
+    }
+    
+    return parseInt(inverse, 2);
+};
