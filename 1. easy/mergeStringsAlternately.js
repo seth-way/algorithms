264fb6c3a62321e8d47a2218cb7bb3f8b1d6c3c2
seth-way <sethaniel.way@outlook.com>
2021-02-22 08:54:45 -0800

    complete solution for 'mergeAlternately' - O(n) time complexity

---
+++ 1. easy/mergeStringsAlternately.js
@@ -0,0 +1,67 @@
+/*
+https://leetcode.com/problems/merge-strings-alternately/
+
+You are given two strings word1 and word2.
+Merge the strings by adding letters in alternating order,
+starting with word1.
+If a string is longer than the other,
+append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+----------------------------------------------------------------------
+
+Example 1:
+-
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+-
+Example 2:
+-
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+-
+Example 3:
+-
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+
+----------------------------------------------------------------------
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.
+
+*/
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+const mergeAlternately = (word1, word2) => {
+    let result = '';
+    const minLength = Math.min(word1.length, word2.length);
+    
+    for (let i = 0; i < minLength; i += 1) {
+        result += word1[i];
+        result += word2[i];
+    }
+    
+    result += word1.slice(minLength);
+    result += word2.slice(minLength);
+    
+    return result;
+};
